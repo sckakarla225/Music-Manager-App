4c8d9d552e3d153abb6fa9d35c3e26b7a2f4a091
sckakarla225 <sck225@cornell.edu>
2023-06-06 20:47:32 -0400

    setup interface

---
+++ interface/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-lg text-red-700 text-center font-bold">Music Manager App</h1>
+    </div>
+  );
+}
+
+export default App;
