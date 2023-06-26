780dfdb03d58e891ded07f6bde1c56d4b4146e96
sckakarla225 <sck225@cornell.edu>
2023-06-26 00:01:30 -0400

    Update config.ts
    just to test redirect uri for vercel domain

--- interface/src/config.ts
+++ interface/src/config.ts
@@ -1,6 +1,7 @@
 const spotifyConfig = {
   clientId: 'fcdf947ce5f74aea9b5c0d47a3747dd2',
-  redirectUri: 'http://localhost:3000/',
+  // redirectUri: 'http://localhost:3000/',
+  redirectUri: 'https://autotunes.vercel.app/',
 }
 
 export { spotifyConfig };
